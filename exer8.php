6b01fb0a56ac70b985331b6fa096fa138f430e73
Geovanna Santos <geosant.dev@gmail.com>
2026-09-03 20:20:43 -0300

    Add Exercício 8 — Criando uma classe simples

---
+++ exer8.php
@@ -0,0 +1,13 @@
+<?php
+ class Usuario{
+       public $nomeUsuario;
+       public $logado = false;
+       
+       function autenticar()
+       {
+           $this->logado = true;
+           echo "Usuário " . $this->nomeUsuario . " autenticado com sucesso!";
+       }
+
+ }
+?>
